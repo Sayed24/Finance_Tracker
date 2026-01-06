0f92f442cab5dcf5fbb77c42edf2ad593e4604ff
SayedRahim Sadat <56288293+Sayed24@users.noreply.github.com>
2026-01-07 01:26:26 +0300

    Update common.js

--- js/common.js
+++ js/common.js
@@ -1,1 +1,10 @@
+getAll("income", i =>
+  getAll("expenses", e => {
+    const income = i.reduce((s,x)=>s+x.amount,0);
+    const expenses = e.reduce((s,x)=>s+x.amount,0);
+    document.getElementById("totalIncome").textContent = `$${income}`;
+    document.getElementById("totalExpenses").textContent = `$${expenses}`;
+    document.getElementById("balance").textContent = `$${income-expenses}`;
+  })
+);
 
