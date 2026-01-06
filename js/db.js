e13bde6bc8fd6503ca75acbc184b7e69972bfd5d
SayedRahim Sadat <56288293+Sayed24@users.noreply.github.com>
2026-01-07 01:22:09 +0300

    Update db.js

--- js/db.js
+++ js/db.js
@@ -1,1 +1,25 @@
+let db;
+
+const request = indexedDB.open("FinanceDB", 1);
+
+request.onupgradeneeded = e => {
+  db = e.target.result;
+  db.createObjectStore("income", { keyPath: "id" });
+  db.createObjectStore("expenses", { keyPath: "id" });
+};
+
+request.onsuccess = e => db = e.target.result;
+
+function add(store, data) {
+  db.transaction(store, "readwrite").objectStore(store).add(data);
+}
+
+function getAll(store, cb) {
+  const req = db.transaction(store).objectStore(store).getAll();
+  req.onsuccess = () => cb(req.result);
+}
+
+function remove(store, id) {
+  db.transaction(store, "readwrite").objectStore(store).delete(id);
+}
 
