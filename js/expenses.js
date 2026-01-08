b2e3d2206cc1d4b8c296f585679855d1b933448c
SayedRahim Sadat <56288293+Sayed24@users.noreply.github.com>
2026-01-08 13:26:19 +0300

    Update expenses.js

--- js/expenses.js
+++ js/expenses.js
@@ -4,28 +4,31 @@ const list = document.getElementById("expenseList");
 form.onsubmit = e => {
   e.preventDefault();
   const [title, category, amount, date] = form.elements;
-  add("expenses", {
+
+  addItem("expenses", {
     id: crypto.randomUUID(),
     title: title.value,
     category: category.value,
     amount: +amount.value,
     date: date.value
   });
+
   form.reset();
   render();
 };
 
 function render() {
-  getAll("expenses", data => {
+  getAllItems("expenses", data => {
     list.innerHTML = "";
     data.forEach(e => {
       const li = document.createElement("li");
-      li.innerHTML = `${e.title} - $${e.amount}
-        <button onclick="remove('expenses','${e.id}');render()">❌</button>`;
+      li.innerHTML = `
+        ${e.title} ($${e.amount})
+        <button onclick="deleteItem('expenses','${e.id}'); render()">❌</button>
+      `;
       list.appendChild(li);
     });
   });
 }
 
 render();
-
