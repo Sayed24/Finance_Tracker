1ae137aad1f92b18ef8945b0c5921bbfd95b20ba
SayedRahim Sadat <56288293+Sayed24@users.noreply.github.com>
2026-01-08 15:46:02 +0300

    Update common.js

--- js/common.js
+++ js/common.js
@@ -1,10 +1,12 @@
-getAllItems("income", income =>
-  getAllItems("expenses", expenses => {
-    const totalIncome = income.reduce((s, i) => s + i.amount, 0);
-    const totalExpenses = expenses.reduce((s, e) => s + e.amount, 0);
+Promise.all([
+  getAllItems("income"),
+  getAllItems("expenses")
+]).then(([income, expenses]) => {
+  const totalIncome = income.reduce((s, i) => s + i.amount, 0);
+  const totalExpenses = expenses.reduce((s, e) => s + e.amount, 0);
 
-    document.getElementById("totalIncome").textContent = `$${totalIncome}`;
-    document.getElementById("totalExpenses").textContent = `$${totalExpenses}`;
-    document.getElementById("balance").textContent = `$${totalIncome - totalExpenses}`;
-  })
-);
+  document.getElementById("totalIncome").textContent = `$${totalIncome}`;
+  document.getElementById("totalExpenses").textContent = `$${totalExpenses}`;
+  document.getElementById("balance").textContent =
+    `$${totalIncome - totalExpenses}`;
+});
