ef6db3281bab438ac19d4449fe9eb9a371241a9d
SayedRahim Sadat <56288293+Sayed24@users.noreply.github.com>
2026-01-08 15:48:39 +0300

    Update export.js

--- js/export.js
+++ js/export.js
@@ -1,1 +1,15 @@
+function exportCSV(type) {
+  getAllItems(type).then(data => {
+    if (!data.length) return alert("No data");
 
+    const headers = Object.keys(data[0]).join(",");
+    const rows = data.map(r => Object.values(r).join(","));
+    const csv = [headers, ...rows].join("\n");
+
+    const blob = new Blob([csv], { type: "text/csv" });
+    const a = document.createElement("a");
+    a.href = URL.createObjectURL(blob);
+    a.download = `${type}.csv`;
+    a.click();
+  });
+}
