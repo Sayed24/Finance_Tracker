c3127b2d40ff034650094c6b1fa945e4104d38a4
SayedRahim Sadat <56288293+Sayed24@users.noreply.github.com>
2026-01-08 15:09:55 +0300

    Update charts.js

--- js/charts.js
+++ js/charts.js
@@ -1,1 +1,38 @@
+function renderCharts(income, expenses) {
 
+  // Expense Pie
+  const categoryTotals = {};
+  expenses.forEach(e => {
+    categoryTotals[e.category] = (categoryTotals[e.category] || 0) + e.amount;
+  });
+
+  new Chart(document.getElementById("expenseChart"), {
+    type: "pie",
+    data: {
+      labels: Object.keys(categoryTotals),
+      datasets: [{
+        data: Object.values(categoryTotals)
+      }]
+    }
+  });
+
+  // Income vs Expense Bar
+  new Chart(document.getElementById("summaryChart"), {
+    type: "bar",
+    data: {
+      labels: ["Income", "Expenses"],
+      datasets: [{
+        data: [
+          income.reduce((s,i)=>s+i.amount,0),
+          expenses.reduce((s,e)=>s+e.amount,0)
+        ]
+      }]
+    }
+  });
+}
+
+getAllItems("income", income =>
+  getAllItems("expenses", expenses =>
+    renderCharts(income, expenses)
+  )
+);
